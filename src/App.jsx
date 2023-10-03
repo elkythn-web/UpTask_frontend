4577ba666ecf1e25f85171c3c59c89426693924b
elkythn-web <elkinportillo5@gmail.com>
2023-10-02 23:11:16 -0600

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import { ProyectosProvider } from "./context/ProyectosProvider";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/index.html">
+    <BrowserRouter>
       <AuthProvider>
         <ProyectosProvider>  
         <Routes>
